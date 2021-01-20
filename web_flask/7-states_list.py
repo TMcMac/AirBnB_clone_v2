a14c2f4d37c7558bbf2b7b36d608855c1720f720
TMcMac <timmcmackenjr@gmail.com>
2021-01-20 20:12:31 +0000

    Adds files for number 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Script to use flask to get cities by statefrom sql db"""
+
+if __name__ == '__main__':
+    from flask import Flask, render_template
+    from models import storage
+    from models.state import State
+
+    app = Flask(__name__)
+    
+    @app.teardown_appcontext
+    def teardown(self):
+        """Call close"""
+        storage.close()
+
+    app.route('/states_list', strict_slashes=False)
+    def state_list():
+        """
+        call a page that presents all state objects in storage.all
+        and presents them in an unordered list in alpha order
+        """
+        states = storage.all("States").values
+        return render_template('7-states_list.html', states=states)
+
+
+    app.run(host='0.0.0.0',port='5000')
